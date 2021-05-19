3de41ef7cbfcecceb1d0314414ee243cc80d4150
Chiara Tozzi <chiara.tozzi2@studio.unibo.it>
2021-05-19 22:21:11 +0200

    Created Class Client

---
+++ TozziSrc/Client.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TozziSrc
+{
+    public class Client
+    {
+        private string name;
+        private string surname;
+        private string id;
+
+        public Client(String i)
+        {
+            String[] parts = i.Split(".");
+            this.name = parts[0];
+            this.surname = parts[1];
+            this.id = parts[2];
+        }
+
+        public Client(string n, string s, string i)
+        {
+            this.name = n;
+            this.surname = s;
+            this.id = i;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public string Surname
+        {
+            get { return this.Surname; }
+            set { this.Surname = value; }
+        }
+
+        public string Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+    }
+}
