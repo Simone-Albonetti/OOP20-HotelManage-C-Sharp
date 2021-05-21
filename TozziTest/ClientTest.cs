b01ad56f6e82eb6cb87c73eef3bfe2669e279d90
Chiara Tozzi <chiara.tozzi2@studio.unibo.it>
2021-05-21 18:12:30 +0200

    Reorganization of tests

--- TozziTest/ClientTest.cs
+++ TozziTest/ClientTest.cs
@@ -10,26 +10,40 @@ namespace TozziTest
     {
  
         [TestMethod]
-        public void TestMethod1()
+        public void TestSearch()
         {
-            ControllerClient c = new TozziSrc.ControllerClient();
-
+            ControllerClient c = new ControllerClient();
             c.InsertClient("luigi", "bianchi", "ccc");
-            c.InsertClient("chiara","tozzi","lfr");
+            c.InsertClient("chiara", "tozzi", "lfr");
+
             Assert.IsFalse(c.SearchClient("ghrt"));
             Assert.IsTrue(c.SearchClient("lfr"));
             Assert.IsTrue(c.SearchClient("ccc"));
+        }
 
-            c.DeleteClient("lfr");
+        [TestMethod]
+        public void TestDelete()
+        {
+            ControllerClient c = new ControllerClient();
+            c.InsertClient("luigi", "bianchi", "ccc");
+            c.InsertClient("chiara", "tozzi", "lfr");
+
+            Assert.IsTrue(c.DeleteClient("lfr"));
             Assert.IsFalse(c.SearchClient("lfr"));
             Assert.IsTrue(c.SearchClient("ccc"));
+        }
+
+        [TestMethod]
+        public void TestGet()
+        {
+            ControllerClient c = new TozziSrc.ControllerClient();
+            c.InsertClient("luigi", "bianchi", "ccc");
+            c.InsertClient("chiara", "tozzi", "lfr");
 
             Client cli = c.GetClient("ccc");
             Client cli1 = c.GetClient("tre");
             Assert.IsNotNull(cli);
             Assert.IsNull(cli1);
-
-            Console.WriteLine("Prova console");
         }
     }
 }
